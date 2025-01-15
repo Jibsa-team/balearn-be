f73e1a1de9533053dd67c83d1a980f3dee179017
HMWG <nurung22222@gmail.com>
2025-01-15 14:12:35 +0900

    feat : team list 조회 api 추가

--- src/main/kotlin/com/jipsa/balearn/api/team/TeamApi.kt
+++ src/main/kotlin/com/jipsa/balearn/api/team/TeamApi.kt
@@ -3,6 +3,7 @@ package com.jipsa.balearn.api.team
 import com.jipsa.balearn.api.global.annotation.CurrentUser
 import com.jipsa.balearn.api.team.dto.TeamCreateRequest
 import com.jipsa.balearn.api.team.dto.TeamCreateResponse
+import com.jipsa.balearn.api.team.dto.TeamReadResponse
 import com.jipsa.balearn.api.team.dto.TeamResponse
 import com.jipsa.balearn.common.api.ApiResponse
 import com.jipsa.balearn.common.dto.File
@@ -42,5 +43,12 @@ class TeamApi(
         @CurrentUser user: User
     ): ApiResponse<TeamResponse> {
         return ApiResponse.success(teamService.readTeam(TeamId(teamId), user.id))
+    }
+
+    @GetMapping("/list")
+    fun readMyTeams(
+        @CurrentUser user: User
+    ): ApiResponse<List<TeamReadResponse>> {
+        return ApiResponse.success(teamService.readMyTeams(user.id).map { TeamReadResponse.from(it) })
     }
 }
