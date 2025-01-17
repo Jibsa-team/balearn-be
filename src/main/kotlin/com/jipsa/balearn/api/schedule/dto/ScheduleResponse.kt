3f47d497f3bd8122067b665ea369b4dc42c38eae
HMWG <nurung22222@gmail.com>
2025-01-17 18:46:04 +0900

    feat : 일정 추가 api 추가

---
+++ src/main/kotlin/com/jipsa/balearn/api/schedule/dto/ScheduleResponse.kt
@@ -0,0 +1,26 @@
+package com.jipsa.balearn.api.schedule.dto
+
+import com.jipsa.balearn.api.mission.dto.MissionReadResponse
+import com.jipsa.balearn.domain.mission.Mission
+import com.jipsa.balearn.domain.schedule.Schedule
+import java.time.LocalDateTime
+
+data class ScheduleResponse(
+    val id: Long,
+    val address: String,
+    val time: LocalDateTime,
+    val topic: String,
+    val mission: List<MissionReadResponse>
+) {
+    companion object {
+        fun from(schedule: Schedule, missions: List<Mission>): ScheduleResponse {
+            return ScheduleResponse(
+                id = schedule.id.value,
+                address = schedule.scheduleInfo.address,
+                time = schedule.scheduleInfo.time,
+                topic = schedule.scheduleInfo.topic,
+                mission = missions.map { MissionReadResponse.from(it) }
+            )
+        }
+    }
+}
