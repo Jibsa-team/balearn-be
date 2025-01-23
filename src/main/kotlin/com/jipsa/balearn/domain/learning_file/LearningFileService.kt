b32c876dcdb62a842b17d8478d9b2dcf9a4c218b
HMWG <nurung22222@gmail.com>
2025-01-23 19:30:05 +0900

    feat : 학습자료 서비스 추가

---
+++ src/main/kotlin/com/jipsa/balearn/domain/learning_file/LearningFileService.kt
@@ -0,0 +1,82 @@
+package com.jipsa.balearn.domain.learning_file
+
+import com.jipsa.balearn.api.learning_file.dto.LearningFileResponse
+import com.jipsa.balearn.common.dto.File
+import com.jipsa.balearn.domain.team.TeamId
+import com.jipsa.balearn.domain.team.TeamReader
+import com.jipsa.balearn.domain.team_user.TeamUserReader
+import com.jipsa.balearn.domain.team_user.TeamUserValidator
+import com.jipsa.balearn.domain.user.User
+import org.springframework.data.domain.Pageable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class LearningFileService(
+    private val learningFileAppender: LearningFileAppender,
+    private val fileAppender: FileAppender,
+    private val teamUserReader: TeamUserReader,
+    private val teamReader: TeamReader,
+    private val learningFileReader: LearningFileReader,
+    private val teamUserValidator: TeamUserValidator,
+    private val learningFileDeleter: LearningFileDeleter
+) {
+    @Transactional
+    fun appendLearningFile(name: String, file: File, user: User, teamId: TeamId): LearningFileResponse {
+        val team = teamReader.read(teamId)
+        val teamUser = teamUserReader.readBy(teamId, user.id)
+
+        val fileUrl = fileAppender.append(file)
+
+        val learningFile = LearningFile(
+            team = team,
+            _learningFileInfo = LearningFIleInfo(
+                name = name,
+                fileUrl = fileUrl,
+                size = file.size,
+                type = file.contentType
+            )
+        )
+        return LearningFileResponse.from(
+            learningFile = learningFileAppender.append(learningFile),
+            createdBy = teamUser,
+            modifiedBy = teamUser
+        )
+    }
+
+    @Transactional
+    fun deleteLearningFile(learningFileId: LearningFileId, user: User) {
+        val learningFile = learningFileReader.read(learningFileId)
+
+        try {
+            teamUserValidator.validLeader(learningFile.team.id, user.id)
+        } catch (e: Exception) {
+            learningFile.isCreator(user.id)
+        }
+
+        learningFileDeleter.delete(learningFile)
+    }
+
+    fun readLearningFile(learningFileId: LearningFileId, user: User): LearningFileResponse {
+        val learningFile = learningFileReader.read(learningFileId)
+        teamUserValidator.validTeamUser(learningFile.team.id, user.id)
+
+        return LearningFileResponse.from(
+            learningFile = learningFile,
+            createdBy = learningFile.createdBy?.let { teamUserReader.readBy(learningFile.team.id, it) },
+            modifiedBy = learningFile.modifiedBy?.let { teamUserReader.readBy(learningFile.team.id, it) }
+        )
+    }
+
+    fun readLearningFilesPage(teamId: TeamId, user: User, pageable: Pageable): List<LearningFileResponse> {
+        teamUserValidator.validTeamUser(teamId, user.id)
+
+        return learningFileReader.readAllBy(teamId, pageable).map { file ->
+            LearningFileResponse.from(
+                learningFile = file,
+                createdBy = file.createdBy?.let { teamUserReader.readBy(teamId, it) },
+                modifiedBy = file.modifiedBy?.let { teamUserReader.readBy(teamId, it) }
+            )
+        }
+    }
+}
