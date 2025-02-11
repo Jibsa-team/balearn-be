e978bb44c91108cc1c11d30edf1b6b2a4698bf56
HMWG <nurung22222@gmail.com>
2025-02-11 18:17:23 +0900

    feat : redis pub/sub template 수정

--- src/main/kotlin/com/jipsa/balearn/infra/redis/ChatPublisher.kt
+++ src/main/kotlin/com/jipsa/balearn/infra/redis/ChatPublisher.kt
@@ -1,17 +1,18 @@
 package com.jipsa.balearn.infra.redis
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.jipsa.balearn.common.constants.BalearnConstants
 import com.jipsa.balearn.domain.chat.Chat
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
 
 @Component
 class ChatPublisher(
-    @Qualifier("anyRedisTemplate")
-    private val redisTemplate: RedisTemplate<String, Any>,
+    private val redisTemplate: RedisTemplate<String, String>,
+    private val objectMapper: ObjectMapper
 ) {
     fun publish(chat: Chat) {
-        redisTemplate.convertAndSend(BalearnConstants.CHAT_TOPIC, chat)
+        val jsonChat = objectMapper.writeValueAsString(chat)
+        redisTemplate.convertAndSend(BalearnConstants.CHAT_TOPIC, jsonChat)
     }
 }
